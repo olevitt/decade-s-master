94701292780a236c8b381ad767754f036412a978
Kimor <agopian@intechinfo.fr>
2019-03-13 13:49:30 +0100

    Hello World

---
+++ api/src/main/java/fr/agopiantexier/Main.java
@@ -0,0 +1,14 @@
+package fr.agopiantexier;
+
+import io.javalin.Javalin;
+
+public class Main {
+    public static void main(String[] args){
+        Javalin app = Javalin.create().start(7000);
+
+        app.get("/", ctx -> ctx.result("Hello World"));
+        
+
+
+    }
+}
